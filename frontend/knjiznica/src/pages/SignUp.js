81072c68dee28ac8643590467027bc99a85801c2
kprovic <kprovic@gmail.com>
2023-12-18 23:07:12 +0100

    povezivanje s bazon

--- frontend/knjiznica/src/pages/SignUp.js
+++ frontend/knjiznica/src/pages/SignUp.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 export function SignUp() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (values) => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/register",
+        {
+          ...values,
+        },
+        { withCredentials: true }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div class="bg-gradient-to-tr from-green-300 to-green-600 h-screen w-full flex justify-center items-center">
       <div class="bg-green-600 w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded">
@@ -13,6 +31,7 @@ export function SignUp() {
           <form action="#" class="w-full flex flex-col justify-center">
             <div class="mb-4">
               <input
+                {...register("name")}
                 type="name"
                 placeholder="Name"
                 class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
@@ -20,6 +39,7 @@ export function SignUp() {
             </div>
             <div class="mb-4">
               <input
+                {...register("lastName")}
                 type="lastname"
                 placeholder="Last name"
                 class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
@@ -27,6 +47,7 @@ export function SignUp() {
             </div>
             <div class="mb-4">
               <input
+                {...register("email")}
                 type="email"
                 placeholder="Email"
                 class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
@@ -34,12 +55,17 @@ export function SignUp() {
             </div>
             <div class="mb-4">
               <input
+                {...register("password")}
                 type="password"
                 placeholder="Password"
                 class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
               />
             </div>
-            <button class="bg-green-600 font-bold text-white focus:outline-none rounded p-3">
+            <button
+              class="bg-green-600 font-bold text-white focus:outline-none rounded p-3"
+              type="submit"
+              onClick={handleSubmit(onSubmit)}
+            >
               Submit
             </button>
           </form>
