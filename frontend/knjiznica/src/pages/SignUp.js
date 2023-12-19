312c4c4c21c3d8c0485ff4e36dc21baf047d205f
aivancevic <antonela.ivancevic@gmail.com>
2023-12-19 23:04:33 +0100

    Login backend i frontend

--- frontend/knjiznica/src/pages/SignUp.js
+++ frontend/knjiznica/src/pages/SignUp.js
@@ -20,57 +20,57 @@ export function SignUp() {
   };
 
   return (
-    <div class="bg-gradient-to-tr from-green-300 to-green-600 h-screen w-full flex justify-center items-center">
-      <div class="bg-green-600 w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded">
-        <div class="w-full md:w-1/2 hidden md:flex flex-col justify-center items-center text-white">
-          <h1 class="text-3xl">Hello</h1>
-          <p class="text-5xl font-extrabold">Welcome!</p>
-        </div>
-        <div class="bg-white w-full md:w-1/2 flex flex-col items-center py-32 px-8">
-          <h3 class="text-3xl font-bold text-green-600 mb-4">LOGIN</h3>
-          <form action="#" class="w-full flex flex-col justify-center">
-            <div class="mb-4">
-              <input
-                {...register("name")}
-                type="name"
-                placeholder="Name"
-                class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
-              />
-            </div>
-            <div class="mb-4">
-              <input
-                {...register("lastName")}
-                type="lastname"
-                placeholder="Last name"
-                class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
-              />
-            </div>
-            <div class="mb-4">
-              <input
-                {...register("email")}
-                type="email"
-                placeholder="Email"
-                class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
-              />
-            </div>
-            <div class="mb-4">
-              <input
-                {...register("password")}
-                type="password"
-                placeholder="Password"
-                class="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-green-600"
-              />
-            </div>
-            <button
-              class="bg-green-600 font-bold text-white focus:outline-none rounded p-3"
-              type="submit"
-              onClick={handleSubmit(onSubmit)}
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-      </div>
+    <div className="bg-gradient-to-tr from-blue-800 to-blue-900 h-screen w-full flex justify-center items-center">
+  <div className="bg-blue-900 w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded">
+    <div className="w-full md:w-1/2 hidden md:flex flex-col justify-center items-center text-white">
+      <h1 className="text-3xl">Hello</h1>
+      <p className="text-5xl font-extrabold">Welcome!</p>
     </div>
+    <div className="bg-white w-full md:w-1/2 flex flex-col items-center py-32 px-8">
+      <h3 className="text-3xl font-bold text-blue-800 mb-4">LOGIN</h3>
+      <form action="#" className="w-full flex flex-col justify-center">
+        <div className="mb-4">
+          <input
+            {...register("name")}
+            type="name"
+            placeholder="Name"
+            className="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-blue-600"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            {...register("lastName")}
+            type="lastname"
+            placeholder="Last name"
+            className="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-blue-600"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            {...register("email")}
+            type="email"
+            placeholder="Email"
+            className="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-blue-600"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            {...register("password")}
+            type="password"
+            placeholder="Password"
+            className="w-full p-3 rounded border placeholder-gray-400 focus:outline-none focus:border-blue-600"
+          />
+        </div>
+        <button
+          className="bg-blue-600 font-bold text-white focus:outline-none rounded p-3"
+          type="submit"
+          onClick={handleSubmit(onSubmit)}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
+</div>
   );
 }
