fda555cc06ad8487fcee8cf1b0eebaf1e046a69f
kprovic <kprovic@gmail.com>
2023-12-20 12:14:08 +0100

    popravak user scheme

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -6,13 +6,17 @@ const userSchema = new mongoose.Schema({
   lastName: { type: String },
   email: { type: String, unique: true },
   password: { type: String },
-  admin: { type: Boolean, default: false },
+  enum: ["admin", "user", "knjiznicar"],
+  default: "user",
 });
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("Users", userSchema);
