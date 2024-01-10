12817ef1682c9d342aec6f1bd8e0d76ad7750f2f
kprovic <kprovic@gmail.com>
2024-01-10 12:24:56 +0100

    prikaz detalja knjiga

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,48 @@
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const registerRoute = require('./routes/RegisterRoute')
-const loginRoute = require('./routes/LoginRoute')
-const getUserByIdRoute = require('./routes/GetUserInfoRoute')
-const getAllUsersRoute = require('./routes/GetAllUsersRoute')
-const deleteUserByIdRoute = require('./routes/DeleteUserById')
-const addBook = require('./routes/AddBook')
-const getAllBooksRoute = require('./routes/GetAllBooksRoute')
-const updateUserByIdRoute = require('./routes/UpdateUserById')
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const registerRoute = require("./routes/RegisterRoute");
+const loginRoute = require("./routes/LoginRoute");
+const getUserByIdRoute = require("./routes/GetUserInfoRoute");
+const getAllUsersRoute = require("./routes/GetAllUsersRoute");
+const deleteUserByIdRoute = require("./routes/DeleteUserById");
+const addBook = require("./routes/AddBook");
+const getAllBooksRoute = require("./routes/GetAllBooksRoute");
+const updateUserByIdRoute = require("./routes/UpdateUserById");
+const getBook = require("./routes/GetBook");
 
-const app = express()
+const app = express();
 
-const PORT = 4000
+const PORT = 4000;
 const DB_URL =
-  'mongodb+srv://admin:Ovojesifra@agilnoknjiznica.rcecdmi.mongodb.net/knjiznica'
+  "mongodb+srv://admin:Ovojesifra@agilnoknjiznica.rcecdmi.mongodb.net/knjiznica";
 
 //create server
 app.listen(PORT, () => {
-  console.log('server started')
-})
+  console.log("server started");
+});
 
 //connect to db
 mongoose
   .connect(DB_URL)
   .then(() => {
-    console.log('db connected')
+    console.log("db connected");
   })
   .catch((err) => {
-    console.log(err.message)
-  })
+    console.log(err.message);
+  });
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }))
-app.use(cookieParser())
-app.use(express.json())
-app.use('/', registerRoute)
-app.use('/', loginRoute)
-app.use('/', getUserByIdRoute)
-app.use('/', getAllUsersRoute)
-app.use('/', deleteUserByIdRoute)
-app.use('/', addBook)
-app.use('/', updateUserByIdRoute)
-app.use('/', updateUserByIdRoute)
-app.use('/', getAllBooksRoute)
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", registerRoute);
+app.use("/", loginRoute);
+app.use("/", getUserByIdRoute);
+app.use("/", getAllUsersRoute);
+app.use("/", deleteUserByIdRoute);
+app.use("/", addBook);
+app.use("/", updateUserByIdRoute);
+app.use("/", updateUserByIdRoute);
+app.use("/", getAllBooksRoute);
+app.use("/", getBook);
